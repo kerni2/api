28e924630f9a10a3db3b260d2fb50789855bbea9
kerni2 <kerni2@ya.ru>
2024-01-07 13:42:25 +0300

    add weather controller

---
+++ app/api/v1/weather.rb
@@ -0,0 +1,26 @@
+# Top level module
+module V1
+  # Weather API
+  class Weather < Grape::API
+    CITY = 'miami'
+
+    before do
+      def client
+        @client ||= ::AccueWeather::Client.new('weather app')
+      end
+
+      def uniq_id
+        @uniq_id ||= client.uniq_id(city: CITY).uniq_id
+      end
+    end
+
+    resource :weather do
+      desc 'get current temperature'
+      get '/current' do
+        result = client.current_temperature(uniq_id: uniq_id)
+
+        present '200'
+      end
+    end
+  end
+end
